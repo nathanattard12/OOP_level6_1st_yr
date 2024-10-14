92f496e41c1a93e3cff9ef07d785124b26c6c68a
Nathan Attard <natahattard@live.com>
2024-10-14 12:50:30 +0200

    14th October

--- Week3_Ploymorphism/Program.cs
+++ Week3_Ploymorphism/Program.cs
@@ -10,7 +10,12 @@ namespace Week3_Ploymorphism
     {
         static void Main(string[] args)
         {
-
+            //Show the user the menu CREATE SHAPE 2. Find per 3.Find area 4.FInd volumee
+            //if user == 1 another menu to  choose to create a circle sphere or sylinder
+            // you add the shape to list
+            //get back to main menu
+            //if then the user chooses find perminter, all perimters of all shapes in list most be shown on screen
+            //depneding on what the user shape choose ank for details
 
         }
     }
